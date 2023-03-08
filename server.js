b5ba7b1157b7059dac10f6c95c79fe51b786e495
Jonathan Cammack <jonathanrcammack@gmail.com>
2023-03-07 18:48:43 -0600

    fixing build process on heroku

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require('express'),
     app = express();
 
-app.use(express.static('www'));
+app.use(express.static('dist'));
 
 // CORS (Cross-Origin Resource Sharing) headers to support Cross-site HTTP requests
 app.all('*', function(req, res, next) {
@@ -13,7 +13,7 @@ app.all('*', function(req, res, next) {
 // API Routes
 // app.get('/blah', routeHandler);
 
-app.set('port', process.env.PORT || 5000);
+app.set('port', process.env.PORT || 4200);
 
 app.listen(app.get('port'), function () {
     console.log('Express server listening on port ' + app.get('port'));

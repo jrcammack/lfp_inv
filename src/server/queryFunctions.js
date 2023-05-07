ba4e871e0869f6bfd5f97e1882dbded976970212
Jonathan Cammack <jonathanrcammack@gmail.com>
2023-05-06 22:15:20 -0500

    reformat
    ran prettier to reformat code

--- src/server/queryFunctions.js
+++ src/server/queryFunctions.js
@@ -1,31 +1,44 @@
 function enterElectronicsItem(requestContent) {
-    let sql = ''
+  let sql = "";
 }
 
 function enterBulkItems(reqData) {
-    let i = 1;
-    let sql = 'INSERT INTO product(product_sku, sub_category_id, shipment_id, has_details) VALUES ';
-    let valuesTxt = '';
-    for (let j = 1; j <= reqData.skuList.length; j++) {
-        valuesTxt += '($' + i + ', ' + '$' + (i + 1) + ', ' + '$' + (i + 2) + ', ' + '$' + (i + 3) + ')';
-        if ((j + 1) > reqData.skuList.length) {
-            valuesTxt += ';'
-        } else {
-            valuesTxt += ', '
-        }
-        i += 4;
+  let i = 1;
+  let sql =
+    "INSERT INTO product(product_sku, sub_category_id, shipment_id, has_details) VALUES ";
+  let valuesTxt = "";
+  for (let j = 1; j <= reqData.skuList.length; j++) {
+    valuesTxt +=
+      "($" +
+      i +
+      ", " +
+      "$" +
+      (i + 1) +
+      ", " +
+      "$" +
+      (i + 2) +
+      ", " +
+      "$" +
+      (i + 3) +
+      ")";
+    if (j + 1 > reqData.skuList.length) {
+      valuesTxt += ";";
+    } else {
+      valuesTxt += ", ";
     }
-    return sql + valuesTxt;
+    i += 4;
+  }
+  return sql + valuesTxt;
 }
 
 function editItem(reqBody) {
-    let sql = '';
-    switch(reqBody.prodContext.subCatContext.categoryID) {
-        case 17:
-        sql = 'UPDATE product_detail SET'    
-    }
+  let sql = "";
+  switch (reqBody.prodContext.subCatContext.categoryID) {
+    case 17:
+      sql = "UPDATE product_detail SET";
+  }
 
-    return sql;
+  return sql;
 }
 
 module.exports = { enterElectronicsItem, enterBulkItems, editItem };
